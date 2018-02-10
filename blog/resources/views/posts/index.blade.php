f97b5f62fef5e1a0da5c08e10805d32d63f1cb02
Lucas Augusto Mendes Rodrigues <lucas.augustus@gmail.com>
2018-02-10 16:45:32 -0200

    Fim da aula 16

---
+++ blog/resources/views/posts/index.blade.php
@@ -0,0 +1,49 @@
+@extends('main')
+
+@section('title', '| Todos os posts')
+
+@section('content')
+
+<div class="row">
+    <div class="col-md-10">
+        <h1>Todos os posts do blog</h1>
+    </div>
+
+    <div class="col-md-2">
+        <a href="{{ route('posts.create') }}" class="btn btn-primary btn-lg btn-block btn-h1-spacing">Criar novo Post</a>
+    </div>
+
+    <div class="col-md-12">
+        <hr>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col-md-12">
+        <table class="table">
+            <thead>
+                <th>#</th>
+                <th>Title</th>
+                <th>Body</th>
+                <th>Created at</th>
+                <th></th>
+            </thead>
+
+            <tbody>
+
+                @foreach ($posts as $post)
+                    <tr>
+                        <th>{{$post->id}}</th>
+                        <td>{{$post->title}}</td>
+                        <td>{{substr($post->body, 0, 50)}} {{ strlen($post->body) > 50 ? "..." : ""}}</td>
+                        <td>{{ date('M j, Y',strtotime($post->created_at) )}}</td>
+                        <td><a href="{{ route('posts.show', $post->id) }}" class="btn btn-default">View</a><a href="{{ route('posts.edit', $post->id) }}" class="btn btn-default">Edit</a></td>
+                    </tr>
+                @endforeach
+
+            </tbody>
+        </table>
+    </div>
+</div>
+
+@endsection
--- blog/resources/views/posts/show.blade.php
+++ blog/resources/views/posts/show.blade.php
@@ -4,7 +4,36 @@
 
 @section('content')
 
-   <h1> {{$post->title}} </h1>
-   <p> {{$post->body}} </p>
+    <div class="row">
+        <div class="col-md-8">
+            <h1> {{$post->title}} </h1>
+            <p> {{$post->body}} </p>
+        </div>
+
+        <div class="col-md-4">
+            <div class="well">
+                <dl class="dl-horizontal">
+                    <dt>Create at:</dt>
+                    <dd>{{ date('M j, Y H:i', strtotime($post->created_at)) }}</dd>
+                </dl>
+                <dl class="dl-horizontal">
+                    <dt>Last update at:</dt>
+                    <dd>{{ date('M j, Y H:i', strtotime($post->updated_at)) }}</dd>
+                </dl>
+
+                <div class="row">
+                    <div class="col-sm-6">
+                        <a href="#" class="btn btn-primary btn-block">Editar</a>
+                    </div>
+
+                    <div class="col-sm-6">
+                        <a href="#" class="btn btn-danger btn-block">Deletar</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+   
 
 @endsection
